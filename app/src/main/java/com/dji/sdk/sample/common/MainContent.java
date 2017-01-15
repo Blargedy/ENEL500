a05254561696058246e5e8be3a1173490cd506d5
Julia John <juliavjohn1@gmail.com>
2017-01-15 11:05:36 -0700

    Ripping out lots of DJI stuff

--- app/src/main/java/com/dji/sdk/sample/common/MainContent.java
+++ app/src/main/java/com/dji/sdk/sample/common/MainContent.java
@@ -4,9 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.os.Looper;
 import android.os.Message;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -18,103 +15,69 @@ import android.widget.TextView;
 import com.dji.sdk.sample.R;
 import com.dji.sdk.sample.utils.DJIDialog;
 
+import dji.common.error.DJIError;
 import dji.sdk.base.DJIBaseProduct;
 import dji.sdk.products.DJIAircraft;
-import dji.sdk.sdkmanager.DJIBluetoothProductConnector;
+import dji.sdk.flightcontroller.DJIFlightController;
+import dji.common.util.DJICommonCallbacks.DJICompletionCallback;
 import dji.thirdparty.eventbus.EventBus;
 
 /**
  * Created by dji on 15/12/18.
  */
-public class MainContent extends RelativeLayout implements DJIBaseProduct.DJIVersionCallback {
-
+public class MainContent extends RelativeLayout
+{
     public static final String TAG = MainContent.class.getName();
 
-    public MainContent(Context context, AttributeSet attrs) {
+    private TextView mTextConnectionStatus;
+    private Button mButtonHoverNow;
+    private Button mButtonLand;
+    private DJIBaseProduct mProduct;
+
+    public MainContent(Context context, AttributeSet attrs)
+    {
         super(context, attrs);
     }
 
-    private TextView mTextConnectionStatus;
-    private TextView mTextProduct;
-    private TextView mTextModelAvailable;
-    private Button mBtnOpen;
-    private Button mBtnBluetooth;
-    private static boolean connected = false;
-
-    private static DJIBluetoothProductConnector connector = null;
-
-    private Handler mHandler;
-    private Handler mHandlerUI;
-    private HandlerThread mHandlerThread = new HandlerThread("Bluetooth");
-
-
-    private DJIBaseProduct mProduct;
-
     @Override
-    protected void onFinishInflate() {
+    protected void onFinishInflate()
+    {
         super.onFinishInflate();
         initUI();
     }
 
-    private void initUI() {
+    private void initUI()
+    {
         Log.v(TAG, "initUI");
 
         mTextConnectionStatus = (TextView) findViewById(R.id.text_connection_status);
-        mTextModelAvailable = (TextView) findViewById(R.id.text_model_available);
-        mTextProduct = (TextView) findViewById(R.id.text_product_info);
-        mBtnOpen = (Button) findViewById(R.id.btn_open);
-        mBtnBluetooth = (Button) findViewById(R.id.btn_bluetooth);
-        mBtnBluetooth.setEnabled(false);
-        mHandlerThread.start();
-        final long currentTime = System.currentTimeMillis();
-        mHandler = new Handler(mHandlerThread.getLooper()){
-            @Override
-            public void handleMessage(Message msg){
-                switch(msg.what){
-                    case 0:
-                        //connected = DJISampleApplication.getBluetoothConnectStatus();
-                    connector = DJISampleApplication.getBluetoothProductConnector();
+        mButtonHoverNow = (Button) findViewById(R.id.btn_hover);
+        mButtonLand = (Button) findViewById(R.id.btn_land);
 
-                    if(connector != null){
-                        mBtnBluetooth.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                mBtnBluetooth.setEnabled(true);
-                            }
-                        });
-                        return;
-                    }else if((System.currentTimeMillis()-currentTime)>=5000){
-                        DJIDialog.showDialog(getContext(),"Fetch Connector failed, reboot if you want to connect the Bluetooth");
-                        return;
-                    }else if(connector == null){
-                        sendEmptyMessageDelayed(0, 1000);
-                    }
-                    break;
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                }
-            }
-        };
-        mHandler.sendEmptyMessage(0);
-
-
-        mBtnOpen.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Utils.isFastDoubleClick()) return;
-                EventBus.getDefault().post(new SetViewWrapper(R.layout.content_component_list, R.string.activity_component_list, getContext()));
-            }
-        });
-        mBtnBluetooth.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(Utils.isFastDoubleClick()) return;
-                EventBus.getDefault().post(new SetViewWrapper(R.layout.content_bluetooth, R.string.component_listview_bluetooth,getContext()));
-
-            }
-        });
+//        mButtonHoverNow.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(Utils.isFastDoubleClick()) return;
+//                mProduct = DJISampleApplication.getProductInstance();
+//                DJIFlightController flightController = ((DJIAircraft) mProduct).getFlightController();
+//                flightController.takeOff(null);
+//            }
+//        });
+//
+//        mButtonLand.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(Utils.isFastDoubleClick()) return;
+//                mProduct = DJISampleApplication.getProductInstance();
+//                final DJIFlightController flightController = ((DJIAircraft) mProduct).getFlightController();
+//                flightController.autoLanding(new DJICompletionCallback() {
+//                    @Override
+//                    public void onResult(DJIError error) {
+//                        flightController.turnOffMotors(null);
+//                    }
+//                });
+//            }
+//        });
     }
 
     @Override
@@ -133,24 +96,6 @@ public class MainContent extends RelativeLayout implements DJIBaseProduct.DJIVer
         super.onDetachedFromWindow();
     }
 
-    private void updateVersion() {
-        String version = null;
-        if(mProduct != null) {
-            version = mProduct.getFirmwarePackageVersion();
-        }
-
-        if(version == null) {
-            mTextModelAvailable.setText("N/A"); //Firmware version: 
-        } else {
-            mTextModelAvailable.setText(version); //"Firmware version: " +
-        }
-    }
-
-    @Override
-    public void onProductVersionChange(String oldVersion, String newVersion) {
-        updateVersion();
-    }
-
     protected BroadcastReceiver mReceiver = new BroadcastReceiver() {
 
         @Override
@@ -161,26 +106,20 @@ public class MainContent extends RelativeLayout implements DJIBaseProduct.DJIVer
 
     };
 
-    private void refreshSDKRelativeUI() {
+    private void refreshSDKRelativeUI()
+    {
         mProduct = DJISampleApplication.getProductInstance();
         Log.d(TAG, "mProduct: " + (mProduct == null? "null" : "unnull") );
-        if (null != mProduct && mProduct.isConnected()) {
-            mBtnOpen.setEnabled(true);
 
-            String str = mProduct instanceof DJIAircraft ? "DJIAircraft" : "DJIHandHeld";
-            mTextConnectionStatus.setText("Status: " + str + " connected");
-            mProduct.setDJIVersionCallback(this);
-            updateVersion();
-
-            if (null != mProduct.getModel()) {
-                mTextProduct.setText("" + mProduct.getModel().getDisplayName());
-            } else {
-                mTextProduct.setText(R.string.product_information);
-            }
-        } else {
-            mBtnOpen.setEnabled(false);
-
-            mTextProduct.setText(R.string.product_information);
+        if (mProduct != null && mProduct.isConnected())
+        {
+            mButtonHoverNow.setEnabled(true);
+            mTextConnectionStatus.setText("Status: Connected");
+        }
+        else
+        {
+            mButtonHoverNow.setEnabled(false);
+            mButtonLand.setEnabled(false);
             mTextConnectionStatus.setText(R.string.connection_loose);
         }
     }
