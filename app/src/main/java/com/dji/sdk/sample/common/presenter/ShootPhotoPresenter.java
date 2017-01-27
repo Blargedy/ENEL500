4da5ca7cf5b565aef04a203204a17f4c717c3a63
Julia John <juliavjohn1@gmail.com>
2017-01-26 21:19:28 -0700

    Trying to get photo download working

--- app/src/main/java/com/dji/sdk/sample/common/presenter/ShootPhotoPresenter.java
+++ app/src/main/java/com/dji/sdk/sample/common/presenter/ShootPhotoPresenter.java
@@ -12,16 +12,22 @@ import com.dji.sdk.sample.common.utility.DJISampleApplication;
 import com.dji.sdk.sample.common.utility.I_ApplicationContextManager;
 import com.dji.sdk.sample.common.view.I_ShootPhotoView;
 
+import java.io.File;
+
 import dji.common.camera.DJICameraSettingsDef;
+import dji.common.error.DJICameraError;
 import dji.common.error.DJIError;
 import dji.common.util.DJICommonCallbacks;
 import dji.sdk.camera.DJICamera;
+import dji.sdk.camera.DJIPlaybackManager;
 
 /**
  * Created by Julia on 2017-01-25.
  */
 
-public class ShootPhotoPresenter extends BroadcastReceiver
+public class ShootPhotoPresenter
+        extends BroadcastReceiver
+        implements DJIPlaybackManager.CameraFileDownloadCallback
 {
     I_ApplicationContextManager contextManager_;
     I_ShootPhotoView view_;
@@ -41,11 +47,11 @@ public class ShootPhotoPresenter extends BroadcastReceiver
     {
         view_.shootPhotoButton().setEnabled(false);
         shootPhoto();
+        //downloadPhoto();
     }
 
     private void shootPhoto()
     {
-        DJICameraSettingsDef.CameraMode cameraMode = DJICameraSettingsDef.CameraMode.ShootPhoto;
         DJICamera camera = DJISampleApplication.getProductInstance().getCamera();
         if (camera != null) {
             DJICameraSettingsDef.CameraShootPhotoMode photoMode = DJICameraSettingsDef.CameraShootPhotoMode.Single;
@@ -62,5 +68,60 @@ public class ShootPhotoPresenter extends BroadcastReceiver
                 }
             });
         }
+    }
+
+    private void downloadPhoto()
+    {
+        DJICamera camera = DJISampleApplication.getProductInstance().getCamera();
+        camera.setCameraMode(DJICameraSettingsDef.CameraMode.Playback, new DJICommonCallbacks.DJICompletionCallback() {
+            @Override
+            public void onResult(DJIError error) {
+                if (error == null) {
+                    Toast.makeText(contextManager_.getApplicationContext(),
+                            "change camera mode: success", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(contextManager_.getApplicationContext(),
+                            error.getDescription(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        DJIPlaybackManager playbackManager = camera.getPlayback();
+
+//        DJICameraError error = playbackManager.selectAllFiles();
+//        if (error == null) {
+//            Toast.makeText(contextManager_.getApplicationContext(),
+//                    "select photos: success", Toast.LENGTH_SHORT).show();
+//            downloadPhoto();
+//        } else {
+//            Toast.makeText(contextManager_.getApplicationContext(),
+//                    error.getDescription(), Toast.LENGTH_LONG).show();
+//        }
+
+//        File destination = contextManager_.getApplicationContext().getFilesDir();
+//
+//        playbackManager.downloadSelectedFiles(destination, this);
+    }
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onEnd() {
+        Toast.makeText(contextManager_.getApplicationContext(),
+                "download photos: success", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onError(Exception e) {
+        Toast.makeText(contextManager_.getApplicationContext(),
+                e.getMessage(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onProgressUpdate(int i) {
+
     }
 }
