1988132aa5b440e9819821891b4f6bb5d65a078a
Julia John <juliavjohn1@gmail.com>
2017-02-08 17:18:11 -0700

    Creating main menu

---
+++ app/src/main/java/com/dji/sdk/sample/common/presenter/MainMenuPresenter.java
@@ -0,0 +1,8 @@
+package com.dji.sdk.sample.common.presenter;
+
+/**
+ * Created by Julia on 2017-02-08.
+ */
+
+public class MainMenuPresenter {
+}
---
+++ app/src/main/java/com/dji/sdk/sample/common/view/MainMenuView.java
@@ -0,0 +1,8 @@
+package com.dji.sdk.sample.common.view;
+
+/**
+ * Created by Julia on 2017-02-08.
+ */
+
+public class MainMenuView {
+}
