b2728df69cbb4e9510bd241e3483b44aecc6f22c
Bill Skinner <skinner.bill.j@gmail.com>
2017-02-08 17:36:49 -0700

    Added function definitions to SwitchBackPathGenerator

--- app/src/main/java/com/dji/sdk/sample/common/mission/SwitchBackPathGenerator.java
+++ app/src/main/java/com/dji/sdk/sample/common/mission/SwitchBackPathGenerator.java
@@ -5,12 +5,44 @@ import java.util.Vector;
 
 /**
  * Created by eric on 2017-01-23.
+ * Edited by Bill Skinner on 2017-02-08
  */
 
 public class SwitchBackPathGenerator {
+    /**
+     * Percentage of overlap, used to determine spacing between pictures
+     */
+    private float percentOverlap_ = 80;
+
     public static List<Coordinate> generateSwitchback(Coordinate bottomLeft, Coordinate topRight, float altitude){
         List<Coordinate> switchbackPoints= new Vector<Coordinate>();
+
+
+
         switchbackPoints.add(new Coordinate(51.080157, -114.131933));
         return switchbackPoints;
     }
+
+    /**
+     * Calculates distance along narrow side of rectangular picture taken vertically downward from
+     * specified altitude with a DJI Phantom 4 drone.
+     *
+     * @param altitude altitude in meters
+     * @return approximate width of short edge of picture of ground in meters
+     */
+    private float calculateImageWidth(float altitude) {
+        return 0; //needs to be completed
+    }
+
+    /**
+     * Calculates distance along long side of rectangular picture taken vertically downward from
+     * specified altitude with a DJI Phantom 4 drone.
+     *
+     * @param altitude altitude in meters
+     * @return approximate length of long edge of picture of ground in meters
+     */
+    private float calculateImageLength(float altitude) {
+        return 0; //needs to be completed
+    }
+
 }
